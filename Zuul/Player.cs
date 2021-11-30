bdbe0c8c5015e183df3202895dd991bb82ae6069
Iwan-Bos <iwanbos05alt@gmail.com>
2021-11-30 13:44:08 +0100

    First Commit

---
+++ Zuul/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zuul
+{
+    public class Player
+    {
+        public Room currentRoom;
+        private int health;
+        public Player()
+        {
+            health = 100;
+            currentRoom = null;
+        }
+    }
+}
